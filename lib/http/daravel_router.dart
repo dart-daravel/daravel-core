c4c295ac3aa4841633b208d96469a98216f19523
The Full-Stack Jonin <francis94c@gmail.com>
2024-09-30 04:39:42 +0100

    Domain routing and optimizations

--- lib/http/daravel_router.dart
+++ lib/http/daravel_router.dart
@@ -1,10 +1,23 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+
+class DaravelRouteParams {
+  final List<String> middlewares = [];
+
+  DaravelRouteParams middleware(String middleware) {
+    middlewares.add(middleware);
+    return this;
+  }
+}
 
 class DaravelRoute {
   final String method;
   final String path;
   final Function? handler;
   final List<DaravelRoute> routes;
+  final DaravelRouteParams params = DaravelRouteParams();
 
   DaravelRoute(
     this.method,
@@ -17,38 +30,65 @@ class DaravelRoute {
 class DaravelRouter {
   DaravelRouter();
 
+  String? _domain;
+
   final List<DaravelRoute> _routes = [];
+  final List<DaravelRouter> _routers = [];
 
-  void get(String path, Function handler) {
-    _routes.add(DaravelRoute('GET', path, handler));
+  /// Define a route with the GET method
+  DaravelRouteParams get(String path, Function handler) {
+    final route = DaravelRoute('GET', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void post(String path, Function handler) {
-    _routes.add(DaravelRoute('POST', path, handler));
+  /// Define a route with the POST method
+  DaravelRouteParams post(String path, Function handler) {
+    final route = DaravelRoute('POST', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void put(String path, Function handler) {
-    _routes.add(DaravelRoute('PUT', path, handler));
+  /// Define a route with the PUT method
+  DaravelRouteParams put(String path, Function handler) {
+    final route = DaravelRoute('PUT', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void patch(String path, Function handler) {
-    _routes.add(DaravelRoute('PATCH', path, handler));
+  /// Define a route with the PATCH method
+  DaravelRouteParams patch(String path, Function handler) {
+    final route = DaravelRoute('PATCH', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void all(String path, Function handler) {
-    _routes.add(DaravelRoute('', path, handler));
+  /// Define a route for all HTTP methods
+  DaravelRouteParams all(String path, Function handler) {
+    final route = DaravelRoute('', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void options(String path, Function handler) {
-    _routes.add(DaravelRoute('OPTIONS', path, handler));
+  /// Define a route with the OPTIONS method
+  DaravelRouteParams options(String path, Function handler) {
+    final route = DaravelRoute('OPTIONS', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void head(String path, Function handler) {
-    _routes.add(DaravelRoute('HEAD', path, handler));
+  /// Define a route with the HEAD method
+  DaravelRouteParams head(String path, Function handler) {
+    final route = DaravelRoute('HEAD', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
-  void delete(String path, Function handler) {
-    _routes.add(DaravelRoute('DELETE', path, handler));
+  /// Define a route with the DELETE method
+  DaravelRouteParams delete(String path, Function handler) {
+    final route = DaravelRoute('DELETE', path, handler);
+    _routes.add(route);
+    return route.params;
   }
 
   void group(String path, Function(DaravelRouter) callback) {
@@ -58,13 +98,37 @@ class DaravelRouter {
     _routes.add(DaravelRoute('', path, null, routes: router.routes));
   }
 
+  DaravelRouter domain(String domain) {
+    final router = DaravelRouter();
+    router._domain = domain;
+
+    _routers.add(router);
+
+    return router;
+  }
+
   List<DaravelRoute> get routes => _routes;
 
   /// Get router
   Router get router {
     final router = Router();
     _buildRoutes(_routes, router);
+    if (_routers.isNotEmpty) {
+      for (final r in _routers) {
+        _mountDaravelRouter(r, router);
+      }
+    }
     return router;
+  }
+
+  void _mountDaravelRouter(DaravelRouter daravelRouter, Router router) {
+    if (daravelRouter._routers.isNotEmpty) {
+      for (final r in daravelRouter._routers) {
+        _mountDaravelRouter(r, router);
+      }
+    } else {
+      router.mount('/', daravelRouter.router.call);
+    }
   }
 
   /// Build routes
@@ -74,16 +138,8 @@ class DaravelRouter {
 
     for (final route in routes) {
       if (route.routes.isEmpty) {
-        if (groupPrefix == null) {
-          // Linear routes
-          router.add(route.method, route.path, (req) {
-            return route.handler!(req);
-          });
-        } else {
-          // Grouped routes
-          router.add(route.method, route.path, (req) {
-            return route.handler!(req);
-          });
+        _mountRoute(router, route);
+        if (groupPrefix != null) {
           parentRouter?.mount("$groupPrefix$lastGroupPrefix", router.call);
         }
       } else {
@@ -96,4 +152,38 @@ class DaravelRouter {
       }
     }
   }
+
+  void _mountRoute(Router router, DaravelRoute route) {
+    router.add(route.method, route.path, (
+      Request req, [
+      String? a1,
+      String? a2,
+      String? a3,
+      String? a4,
+      String? a5,
+      String? a6,
+    ]) {
+      return _domain == null
+          ? Function.apply(route.handler!, [req, ...req.params.values])
+          : const Pipeline()
+              .addMiddleware(logRequests())
+              .addMiddleware(_domainHandler(router, _domain!))
+              .addHandler((req) {
+              return Function.apply(
+                  route.handler!, [req, ...req.params.values]);
+            });
+    });
+  }
+}
+
+Middleware _domainHandler(Router? router, String domain) {
+  return (Handler innerHandler) {
+    return (Request request) {
+      final host = request.headers[HttpHeaders.hostHeader] ?? '';
+      if (router != null && host.contains(domain)) {
+        return router.call(request);
+      }
+      return innerHandler(request);
+    };
+  };
 }
