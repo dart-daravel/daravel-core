5cd28dd017b504460405faa10c91f512f616072a
The Full-Stack Jonin <francis94c@gmail.com>
2024-10-01 00:07:51 +0100

    Router test

--- test/router_test.dart
+++ test/router_test.dart
@@ -1,7 +1,32 @@
+import 'package:daravel_core/http/daravel_router.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:daravel_core/daravel_core.dart';
+import 'package:shelf/shelf.dart';
 
 void main() {
-  test('Top level routes', () {});
+  test('Top level routes', () {
+    final router = DaravelRouter();
+    router.get('/', (Request request) => Response.ok('Hello, World!'));
+    router.get('/<name>',
+        (Request request, String name) => Response.ok('Hello, $name!'));
+    expect(router.routes.length, 2);
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/<name>');
+  });
+
+  test('Nested routes Level 1', () {
+    final router = DaravelRouter();
+    router.get('/', (Request request) => Response.ok('Hello, World!'));
+    router.group('/v1', (router) {
+      router.get('/', (Request request) => Response.ok('Hello, World!'));
+      router.get('/echo/<message>',
+          (Request request, String message) => Response.ok(message));
+    });
+    expect(router.routes.length, 2);
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/v1');
+    expect(router.routes[1].routes.length, 2);
+    expect(router.routes[1].routes[0].path, '/');
+    expect(router.routes[1].routes[1].path, '/echo/<message>');
+  });
 }
