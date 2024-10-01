0b7f77fee575fc3b5c0e094cd41f76f8a6e73994
The Full-Stack Jonin <francis94c@gmail.com>
2024-10-01 19:34:15 +0100

    User DartVM for test.

--- test/router_test.dart
+++ test/router_test.dart
@@ -1,15 +1,30 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
+
 import 'package:shelf/shelf.dart';
 
 import 'package:daravel_core/daravel_core.dart';
+import 'package:test/test.dart';
 
 void main() {
-  final host = 'http://localhost:8080';
+  const host = 'http://localhost:8080';
+
   test('Top level routes', () async {
     final router = DaravelRouter();
     router.get('/', (Request request) => Response.ok('Hello, World!'));
     router.get('/<name>',
         (Request request, String name) => Response.ok('Hello, $name!'));
+    router.get(
+        '/<name>/age/<age>',
+        (Request request, String name, String age) =>
+            Response.ok('Hello, $name! You are $age years old!'));
+
+    expect(router.routes.length, 3);
+    expect(router.routes[0].method, 'GET');
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].method, 'GET');
+    expect(router.routes[1].path, '/<name>');
+    expect(router.routes[2].method, 'GET');
+    expect(router.routes[2].path, '/<name>/age/<age>');
 
     final app = DaravelApp(
       routers: [router],
@@ -18,12 +33,7 @@ void main() {
       ],
     );
 
-    expect(router.routes.length, 2);
-    expect(router.routes[0].method, 'GET');
-    expect(router.routes[0].path, '/');
-    expect(router.routes[1].path, '/<name>');
-
-    await app.run();
+    final HttpServer server = await app.run();
 
     final Response response =
         await app.rootHandler!(Request('GET', Uri.parse('$host/')));
@@ -36,17 +46,57 @@ void main() {
 
     expect(response2.statusCode, 200);
     expect(await response2.readAsString(), 'Hello, John!');
+
+    final Response response3 =
+        await app.rootHandler!(Request('GET', Uri.parse('$host/John/age/25')));
+
+    expect(response3.statusCode, 200);
+    expect(
+        await response3.readAsString(), 'Hello, John! You are 25 years old!');
+
+    await server.close();
   });
 
-  test('Post requests', () {
+  test('Post requests', () async {
     final router = DaravelRouter();
     router.post('/', (Request request) => Response.ok('Hello, World!'));
     router.post('/<name>',
         (Request request, String name) => Response.ok('Hello, $name!'));
-    expect(router.routes.length, 2);
+    router.post(
+        '/<name>/age/<age>',
+        (Request request, String name, String age) =>
+            Response.ok('Hello, $name! You are $age years old!'));
+
+    expect(router.routes.length, 3);
     expect(router.routes[0].method, 'POST');
     expect(router.routes[0].path, '/');
+    expect(router.routes[1].method, 'POST');
     expect(router.routes[1].path, '/<name>');
+    expect(router.routes[2].method, 'POST');
+    expect(router.routes[2].path, '/<name>/age/<age>');
+
+    final app = DaravelApp(
+      routers: [router],
+      globalMiddlewares: [
+        LoggerMiddleware(),
+      ],
+    );
+
+    final HttpServer server = await app.run();
+
+    final Response response =
+        await app.rootHandler!(Request('POST', Uri.parse('$host/')));
+
+    expect(response.statusCode, 200);
+    expect(await response.readAsString(), 'Hello, World!');
+
+    final Response response2 =
+        await app.rootHandler!(Request('POST', Uri.parse('$host/John')));
+
+    expect(response2.statusCode, 200);
+    expect(await response2.readAsString(), 'Hello, John!');
+
+    await server.close();
   });
 
   test('Put requests', () {
