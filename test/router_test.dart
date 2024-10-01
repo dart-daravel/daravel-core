b92b55851c3ee11ecac1fc546697a1710b7f4975
The Full-Stack Jonin <francis94c@gmail.com>
2024-10-01 07:44:46 +0100

    More router tests.

--- test/router_test.dart
+++ test/router_test.dart
@@ -10,6 +10,51 @@ void main() {
     router.get('/<name>',
         (Request request, String name) => Response.ok('Hello, $name!'));
     expect(router.routes.length, 2);
+    expect(router.routes[0].method, 'GET');
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/<name>');
+  });
+
+  test('Post requests', () {
+    final router = DaravelRouter();
+    router.post('/', (Request request) => Response.ok('Hello, World!'));
+    router.post('/<name>',
+        (Request request, String name) => Response.ok('Hello, $name!'));
+    expect(router.routes.length, 2);
+    expect(router.routes[0].method, 'POST');
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/<name>');
+  });
+
+  test('Put requests', () {
+    final router = DaravelRouter();
+    router.put('/', (Request request) => Response.ok('Hello, World!'));
+    router.put('/<name>',
+        (Request request, String name) => Response.ok('Hello, $name!'));
+    expect(router.routes.length, 2);
+    expect(router.routes[0].method, 'PUT');
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/<name>');
+  });
+
+  test('Patch requests', () {
+    final router = DaravelRouter();
+    router.patch('/', (Request request) => Response.ok('Hello, World!'));
+    router.patch('/<name>',
+        (Request request, String name) => Response.ok('Hello, $name!'));
+    expect(router.routes.length, 2);
+    expect(router.routes[0].method, 'PATCH');
+    expect(router.routes[0].path, '/');
+    expect(router.routes[1].path, '/<name>');
+  });
+
+  test('Delete requests', () {
+    final router = DaravelRouter();
+    router.delete('/', (Request request) => Response.ok('Hello, World!'));
+    router.delete('/<name>',
+        (Request request, String name) => Response.ok('Hello, $name!'));
+    expect(router.routes.length, 2);
+    expect(router.routes[0].method, 'DELETE');
     expect(router.routes[0].path, '/');
     expect(router.routes[1].path, '/<name>');
   });
