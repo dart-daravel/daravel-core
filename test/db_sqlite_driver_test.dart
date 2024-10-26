eb520a1ae7e929378975e03a43bf5ff41b993f49
The Full-Stack Jonin <francis94c@gmail.com>
2024-10-27 00:07:09 +0100

    test: SQLite default value field

--- test/db_sqlite_driver_test.dart
+++ test/db_sqlite_driver_test.dart
@@ -30,35 +30,54 @@ void main() {
   });
 
   test('Create simple table Blueprint', () {
-    final query = Schema.create('users_1', (table) {
+    final table = 'users_1';
+
+    final query = Schema.create(table, (table) {
       table.increments('id');
       table.string('email');
       table.string('password');
     });
 
     expect(query,
-        'CREATE TABLE users_1 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL);');
+        'CREATE TABLE $table (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL);');
   });
 
   test('Create nullable field table Blueprint', () {
-    final query = Schema.create('users_2', (table) {
+    final table = 'users_2';
+
+    final query = Schema.create(table, (table) {
       table.increments('id');
       table.string('email').nullable();
       table.string('password');
     });
 
     expect(query,
-        'CREATE TABLE users_2 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100), password VARCHAR(100) NOT NULL);');
+        'CREATE TABLE $table (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100), password VARCHAR(100) NOT NULL);');
   });
 
   test('Create unique field table Blueprint', () {
-    final query = Schema.create('users_3', (table) {
+    final table = 'users_3';
+
+    final query = Schema.create(table, (table) {
       table.increments('id');
       table.string('email').unique();
       table.string('password');
     });
 
     expect(query,
-        'CREATE TABLE users_3 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL);');
+        'CREATE TABLE $table (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL);');
+  });
+
+  test('Create default value field table Blueprint', () {
+    final table = 'users_4';
+
+    final query = Schema.create(table, (table) {
+      table.increments('id');
+      table.string('email').unique().defaultsTo('john-doe@example.com');
+      table.string('password');
+    });
+
+    expect(query,
+        'CREATE TABLE $table (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(100) UNIQUE NOT NULL DEFAULT \'john-doe@example.com\', password VARCHAR(100) NOT NULL);');
   });
 }
